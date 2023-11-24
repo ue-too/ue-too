5715324a62d470eb4493058e9c9f1e7f7f6fe089
niuee <vntchang@gmail.com>
2023-11-24 13:43:36 +0800

    Add vCanvas custom element and initial tests

--- src/vCanvas/vCanvas.ts
+++ src/vCanvas/vCanvas.ts
@@ -1,1 +1,49 @@
 export class PlaceHolder{}
+
+export class vCanvas extends HTMLElement {
+    
+    private width: number;
+    private height: number;
+    private _canvas: HTMLCanvasElement;
+    static observedAttributes = ["width", "height", "full-screen", "style"];
+
+    constructor(){
+        super();
+        this._canvas = document.createElement('canvas');
+    }
+
+    attributeChangedCallback(name: string, oldValue: any, newValue: any) {
+        // console.log(`Attribute ${name} has changed.`);
+        if (name == "width"){
+            this.width = +newValue;
+        }
+        if (name == "height"){
+            this.height = +newValue;
+        }
+        if (name == "full-screen"){
+            // console.log("full-screen", newValue);
+            if (newValue !== null && newValue !== "false"){
+                this.width = window.innerWidth;
+                this.height = window.innerHeight;
+                this._canvas.width = window.innerWidth;
+                this._canvas.height = window.innerHeight;
+            }
+        }
+        if (name == "style"){
+            this._canvas.setAttribute(name, newValue);
+        }
+    }
+
+    getInternalCanvas(): HTMLCanvasElement {
+        return this._canvas;
+    }
+
+    connectedCallback(){
+        this.shadowRoot.appendChild(this._canvas);
+        
+    }
+
+    disconnectedCallback(){
+        
+    }
+}
--- tests/customCanvas/vCanvas.test.ts
+++ tests/customCanvas/vCanvas.test.ts
@@ -1,6 +1,52 @@
+import { vCanvas } from "../../src/";
 
 describe("First Kickoff Test", ()=>{
+
     test("First Placeholder Test", ()=>{
         expect(10).toBe(10);
-    })
+    });
+
+
+});
+
+customElements.define('v-canvas', vCanvas);
+
+describe("Custom Canvas Initialization", ()=>{
+
+    let vCanvasElement: vCanvas;
+    
+    beforeEach(()=>{
+        vCanvasElement = document.createElement('v-canvas') as vCanvas;
+    });
+
+    test("Initialize Custom Canvas Element with width and height set to certain size", ()=>{
+        vCanvasElement.setAttribute("width", "300");
+        vCanvasElement.setAttribute("height", "300");
+        expect(vCanvasElement.getInternalCanvas().width).toBe(300);
+        expect(vCanvasElement.getInternalCanvas().width).toBe(300);
+    });
+
+    test("Initialize Custom Canvas Element set with full screen", ()=>{
+        vCanvasElement.setAttribute("full-screen", "true");
+        expect(vCanvasElement.getInternalCanvas().width).toBe(window.innerWidth);
+        expect(vCanvasElement.getInternalCanvas().height).toBe(window.innerHeight);
+    });
+
+    test("Initialize Custom Canvas Element with set style on background color", ()=>{
+        vCanvasElement.setAttribute("style", "background-color: gray");
+        expect(window.getComputedStyle(vCanvasElement.getInternalCanvas(), null).getPropertyValue('background-color')).toBe('gray');
+        expect(window.getComputedStyle(vCanvasElement, null).getPropertyValue('background-color')).toBe("gray");
+    });
+
+});
+
+describe("Custom Canvas Initialization", ()=>{
+    let vCanvasElement: vCanvas;
+    beforeEach(()=>{
+        vCanvasElement = document.createElement('v-canvas') as vCanvas;
+    });
+
+    test("Placeholder test", ()=>{
+
+    });
 });
