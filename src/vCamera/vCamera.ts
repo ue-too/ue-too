84a52ad0c0bd28a1e488b4f068468b2f1f1d009e
niuee <vntchang@gmail.com>
2023-11-24 22:04:34 +0800

    Initial features and test cases

--- src/vCamera/vCamera.ts
+++ src/vCamera/vCamera.ts
@@ -1,39 +1,260 @@
 import { PointCal } from "point2point";
 import { Point } from "../../src";
 
+
+export type CameraOptions = {
+    initialPosition?: Point;
+    initialZoom?: number;
+    initialRotation?: number;
+}
+
+export type Boundaries = {
+    min?: {x?: number, y?: number};
+    max?: {x?: number, y?: number};
+}
+
+const defaultCameraOptions: CameraOptions = {
+    initialPosition: {x: 0, y: 0},
+    initialZoom: 1,
+    initialRotation: 0
+}
+
+
+
 export class vCamera {
 
     private position: Point;
-    private zoom: number;
+    private zoomLevel: number;
     private rotation: number;
+    
+    private boundaries: {min: {x?: number, y?: number}, max: {x?: number, y?: number}};
+    private zoomLevelLimits: {min?: number, max?: number};
 
-    constructor(initialPosition: Point = {x: 0, y: 0}, initialZoom: number = 1, initialRotation: number = 0){
-        this.position = initialPosition;
-        this.zoom = initialZoom;
-        this.rotation = initialRotation;
+    private viewPortWidth: number;
+    private viewPortHeight: number;
+
+    constructor(position: Point = {x: 0, y: 0}, viewPortWidth: number = 1000, viewPortHeight: number = 1000, zoomLevel: number =  1, rotation: number = 0){
+        if (!this.zoomLevelValid(zoomLevel)){
+            throw new InvalidZoomLevelError("zoom level cannot be less than or equal to 0");
+        }
+        this.position = position;
+        this.zoomLevel = zoomLevel;
+        this.rotation = rotation;
+        this.viewPortHeight = viewPortHeight;
+        this.viewPortWidth = viewPortWidth;
+    }
+
+    zoomLevelValid(zoomLevel: number){
+        if(zoomLevel <= 0){
+            return false;
+        }
+        return true;
+    }
+
+    setViewPortWidth(width: number){
+        this.viewPortWidth = width;
+    }
+
+    getViewPortWidth(): number{
+        return this.viewPortWidth;
+    }
+
+    setViewPortHeight(height: number){
+        this.viewPortHeight = height;
+    }
+
+    getViewPortHeight(): number{
+        return this.viewPortHeight;
     }
     
-    setPosition(position: Point){
+    setPosition(position: Point): boolean{
+        if(!this.withinBoundaries(position)){
+            return false;
+        }
         this.position = position;
+        return true;
     }
 
     setRotation(rotation: number){
+        rotation = this.normalizeAngleZero2TwoPI(rotation);
         this.rotation = rotation;
     }
+    
+    setRotationDeg(rotationDeg: number){
+        this.setRotation(rotationDeg * Math.PI / 180);
+    }
 
-    setZoom(zooLevel: number){
-        this.zoom = zooLevel;
+    setZoomLevel(zoomLevel: number){
+        if(this.zoomLevelLimits !== undefined && 
+        ((this.zoomLevelLimits.max !== undefined && this.zoomLevelLimits.max < zoomLevel) || 
+         (this.zoomLevelLimits.min !== undefined && this.zoomLevelLimits.min > zoomLevel)
+        )){
+            return false;
+        }
+        this.zoomLevel = zoomLevel;
+        return true;
+    }
+
+    setHorizontalBoundaries(min: number, max: number){
+        if (min > max){
+            let temp = max;
+            max = min;
+            min = temp;
+        }
+        if(this.boundaries == undefined){
+            this.boundaries = {min: {x: undefined, y: undefined}, max: {x: undefined, y: undefined}};
+        }
+        this.boundaries.min.x = min;
+        this.boundaries.max.x = max;
+    }
+
+    setVerticalBoundaries(min: number, max: number){
+        if (min > max){
+            let temp = max;
+            max = min;
+            min = temp;
+        }
+        if(this.boundaries == undefined){
+            this.boundaries = {min: {x: undefined, y: undefined}, max: {x: undefined, y: undefined}};
+        }
+        this.boundaries.min.y = min;
+        this.boundaries.max.y = max;
     }
 
     getPosition(): Point{
         return this.position;
     }
 
-    getZoom(): number{
-        return this.zoom;
+    getZoomLevel(): number{
+        return this.zoomLevel;
     }
 
     getRotation(): number{
         return this.rotation;
+    }
+
+    getRotationDeg(): number{
+        return this.rotation * 180 / Math.PI
+    }
+
+    getBoundaries(): Boundaries | undefined {
+        return this.boundaries;
+    }
+
+    withinBoundaries(point: Point): boolean{
+        if(this.boundaries == undefined){
+            // no boundaries 
+            return true;
+        }
+        let leftSide = false;
+        let rightSide = false;
+        let topSide = false;
+        let bottomSide = false;
+        // check within boundaries horizontally
+        if(this.boundaries.max.x == undefined || point.x <= this.boundaries.max.x){
+            rightSide = true;
+        }
+        if(this.boundaries.min.x == undefined || point.x >= this.boundaries.min.x){
+            leftSide = true;
+        }
+        if(this.boundaries.max.y == undefined || point.y <= this.boundaries.max.y){
+            topSide = true;
+        }
+        if(this.boundaries.min.y == undefined || point.y >= this.boundaries.min.y){
+            bottomSide = true;
+        }
+        return leftSide && rightSide && topSide && bottomSide;
+    }
+
+    move(delta: Point){
+        if(!this.withinBoundaries(PointCal.addVector(this.position, delta))){
+            return false;
+        }
+        this.position = PointCal.addVector(this.position, delta);
+        return true;
+    }
+
+    resetZoomLevel(){
+        this.zoomLevel = 1;
+    }
+
+    normalizeAngleZero2TwoPI(angle: number){
+        // reduce the angle  
+        angle = angle % (Math.PI * 2);
+
+        // force it to be the positive remainder, so that 0 <= angle < 360  
+        angle = (angle + Math.PI * 2) % (Math.PI * 2); 
+        return angle;
+    }
+
+    getAngleSpan(angle: number): number{
+        // in radians
+        angle = this.normalizeAngleZero2TwoPI(angle);
+        let angleDiff = angle - this.rotation;
+        
+        if(angleDiff > Math.PI){
+            angleDiff = - (Math.PI * 2 - angleDiff);
+        }
+
+        if(angleDiff < -Math.PI){
+            angleDiff += (Math.PI * 2);
+        }
+        return angleDiff;
+    }
+
+    getAngleSpanDeg(angle: number): number{
+        // in degrees
+        return this.getAngleSpan(angle * Math.PI / 180) * 180 / Math.PI;
+    }
+
+    spinDeg(deltaAngle: number){
+        // in degrees
+        this.spin(deltaAngle * Math.PI / 180);
+    }
+
+    spin(deltaAngle: number){
+        // in radians
+        this.rotation = this.normalizeAngleZero2TwoPI(this.rotation + deltaAngle);
+    }
+
+    setMaxZoomLevel(maxZoomLevel: number){
+        if(this.zoomLevelLimits == undefined){
+            this.zoomLevelLimits = {min: undefined, max: undefined};
+        }
+        if((this.zoomLevelLimits.min != undefined && this.zoomLevelLimits.min > maxZoomLevel) || this.zoomLevel > maxZoomLevel){
+            return false;
+        }
+        this.zoomLevelLimits.max = maxZoomLevel;
+        return true
+    }
+
+    setMinZoomLevel(minZoomLevel: number){
+        if(this.zoomLevelLimits == undefined){
+            this.zoomLevelLimits = {min: undefined, max: undefined};
+        }
+        if((this.zoomLevelLimits.max != undefined && this.zoomLevelLimits.max < minZoomLevel) || this.zoomLevel < minZoomLevel){
+            return false;
+        }
+        this.zoomLevelLimits.min = minZoomLevel;
+        return true;
+    }
+
+    getZoomLevelLimits(): {min?: number, max?: number} | undefined{
+        return this.zoomLevelLimits;
+    }
+
+    convert2WorldSpace(point: Point): Point{
+        let cameraFrameCenter = {x: this.viewPortWidth / 2, y: this.viewPortHeight / 2};
+        let delta2Point = PointCal.subVector(point, cameraFrameCenter);
+        delta2Point = PointCal.rotatePoint(delta2Point, this.rotation);
+        return PointCal.addVector(this.position, delta2Point);
+    }
+
+}
+
+export class InvalidZoomLevelError extends Error {
+
+    constructor(msg: string){
+        super(msg);
     }
 }
--- tests/camera/vCamera.test.ts
+++ tests/camera/vCamera.test.ts
@@ -1,4 +1,5 @@
-import { vCamera } from "../../src";
+import { getRandomPoint } from "..";
+import { vCamera, InvalidZoomLevelError } from "../../src";
 
 describe("Initialize a vCamera object", ()=>{
     let camera: vCamera;
@@ -7,10 +8,17 @@ describe("Initialize a vCamera object", ()=>{
         camera = new vCamera();
     });
 
-    test("Initialzing with default parameters", ()=>{
+    test("Initialze with default parameters", ()=>{
         expect(camera.getPosition()).toEqual({x: 0, y: 0});
         expect(camera.getRotation()).toBe(0);
-        expect(camera.getZoom()).toBe(1);
+        expect(camera.getZoomLevel()).toBe(1);
+    });
+
+    test("Initialize with invalid zoom level", ()=>{
+        let testFunc = ()=>{
+            camera = new vCamera({x: 0, y: 0}, 100, 100, 0);
+        }
+        expect(testFunc).toThrow(InvalidZoomLevelError);
     });
 
 });
@@ -33,9 +41,193 @@ describe("Basic Operations on vCamera object attributes", ()=>{
         expect(camera.getRotation()).toBeCloseTo(Math.PI / 2);
     });
 
-    test("Set Camera Zoom", ()=>{
-        camera.setZoom(10);
-        expect(camera.getZoom()).toBe(10);
+    test("Set view port width", ()=>{
+        camera.setViewPortWidth(300);
+        expect(camera.getViewPortWidth()).toBe(300);
     });
 
+    test("Set view port height", ()=>{
+        camera.setViewPortHeight(300);
+        expect(camera.getViewPortHeight()).toBe(300);
+    });
+
+});
+
+
+describe("Camera boundaries manipulations", ()=>{
+    let camera: vCamera;
+
+    beforeEach(()=>{
+        camera = new vCamera();
+    });
+
+    test("Set camera translation boundaries", ()=>{
+        camera.setHorizontalBoundaries(-500, 300);
+        camera.setVerticalBoundaries(-500, 300);
+        const boundaries = camera.getBoundaries();
+        expect(boundaries == undefined).toBe(false);
+        if(boundaries !== undefined){
+            expect(boundaries.max).toEqual({x: 300, y: 300});
+            expect(boundaries.min).toEqual({x: -500, y: -500});
+        }
+    });
+
+    test("Set camera translation boundaries with rogue inputs (min greater than max)", ()=>{
+        camera.setHorizontalBoundaries(300, -500);
+        camera.setVerticalBoundaries(300, -500);
+        const boundaries = camera.getBoundaries();
+        expect(boundaries == undefined).toBe(false);
+        if(boundaries !== undefined){
+            expect(boundaries.max).toEqual({x: 300, y: 300});
+            expect(boundaries.min).toEqual({x: -500, y: -500});
+        }
+    });
+
+    test("Check if a point is inside of camera translation boundaries", ()=>{
+        camera.setHorizontalBoundaries(300, -500);
+        camera.setVerticalBoundaries(300, -500);
+        expect(camera.withinBoundaries({x: -700, y: -700})).toBe(false);
+    });
+
+});
+
+describe("Camera translation Movements", ()=>{
+    let camera: vCamera;
+
+    beforeEach(()=>{
+        camera = new vCamera();
+    });
+
+    test("Set camera position outside of boundaries", ()=>{
+        camera.setHorizontalBoundaries(-500, 300);
+        camera.setVerticalBoundaries(-500, 300);
+        const actualRes = camera.setPosition({x: -700, y: -700});
+        expect(actualRes).toBe(false);
+        expect(camera.getPosition()).toEqual({x: 0, y: 0});
+    });
+
+    test("Translate camera given a delta translation vector", ()=>{
+        const moveDeltaVector = {x: 10, y: 10};
+        const actualRes = camera.move(moveDeltaVector);
+        expect(actualRes).toBe(true);
+        expect(camera.getPosition()).toEqual({x: 10, y: 10});
+    });
+
+    test("Translate camera to a destination that is not within translation boundaries", ()=>{
+        const moveDeltaVector = getRandomPoint(400, 500);
+        camera.setHorizontalBoundaries(-500, 300);
+        camera.setVerticalBoundaries(-500, 300);
+        const actualRes = camera.move(moveDeltaVector);
+        expect(actualRes).toBe(false);
+        expect(camera.getPosition()).toEqual({x: 0, y: 0});
+    });
+});
+
+describe("Camera zooming operations", ()=>{
+    let camera: vCamera;
+
+    beforeEach(()=>{
+        camera = new vCamera();
+    });
+
+    test("Set camera zoom", ()=>{
+        camera.setZoomLevel(10);
+        expect(camera.getZoomLevel()).toBe(10);
+
+        camera.setMinZoomLevel(0.005);
+        camera.setMaxZoomLevel(30);
+        let testRes = camera.setZoomLevel(0.003);
+        expect(testRes).toBe(false);
+        testRes = camera.setZoomLevel(35);
+        expect(testRes).toBe(false);
+    });
+
+    test("Reset zoom", ()=>{
+        camera.setZoomLevel(10);
+        camera.resetZoomLevel();
+        expect(camera.getZoomLevel()).toBe(1);
+    });
+
+    test("Set maximum zoom", ()=>{
+        camera.setMaxZoomLevel(10);
+        const zoomLevelLimits = camera.getZoomLevelLimits();
+        expect(zoomLevelLimits == undefined).toBe(false);
+        expect(zoomLevelLimits?.max).toBe(10);
+        camera.setZoomLevel(6);
+        camera.setMinZoomLevel(5);
+        const testRes = camera.setMaxZoomLevel(4);
+        expect(testRes).toBe(false);
+    });
+
+    test("Set minimum zoom", ()=>{
+        camera.setZoomLevel(11);
+        camera.setMinZoomLevel(10);
+        const zoomLevelLimits = camera.getZoomLevelLimits();
+        expect(zoomLevelLimits == undefined).toBe(false);
+        expect(zoomLevelLimits?.min).toBe(10);
+        camera.setMaxZoomLevel(15);
+        const testRes = camera.setMinZoomLevel(20);
+        expect(testRes).toBe(false);
+    });
+
+});
+
+describe("Camera rotation operations", ()=>{
+    let camera: vCamera;
+
+    beforeEach(()=>{
+        camera = new vCamera();
+    });
+
+    test("Set camera rotation in degree", ()=>{
+        camera.setRotationDeg(45);
+        expect(camera.getRotationDeg()).toBeCloseTo(45);
+    });
+
+    test("Set camera rotation with a number that is over 360", ()=>{
+        camera.setRotationDeg(405);
+        expect(camera.getRotationDeg()).toBeCloseTo(45);
+    });
+
+    test("Set camera rotation with a number that is negative", ()=>{
+        camera.setRotationDeg(-45);
+        expect(camera.getRotationDeg()).toBeCloseTo(315);
+    });
+
+    test("Find the smaller angle between the current camera rotation and target rotation", ()=>{
+        expect(camera.getAngleSpanDeg(-45)).toBeCloseTo(-45);
+        expect(camera.getAngleSpanDeg(195)).toBeCloseTo(-165);
+        expect(camera.getAngleSpanDeg(-195)).toBeCloseTo(165);
+        camera.setRotationDeg(-45);
+        expect(camera.getAngleSpanDeg(405)).toBeCloseTo(90);
+    });
+
+    test("Rotate camera with a given delta angle", ()=>{
+        camera.spinDeg(-45);
+        expect(camera.getRotationDeg()).toBeCloseTo(315);
+        camera.spinDeg(360);
+        expect(camera.getRotationDeg()).toBeCloseTo(315);
+        camera.spinDeg(90);
+        expect(camera.getRotationDeg()).toBeCloseTo(45);
+    });
+
+});
+
+
+describe("Interactions with the world", ()=>{
+    let camera: vCamera;
+
+    beforeEach(()=>{
+        camera = new vCamera();
+    });
+
+    test("Convert point within camera view to world space", ()=>{
+        const point = {x: 100, y: 100};
+        camera.setPosition({x: 30, y: 50});
+        camera.setRotationDeg(-45);
+        const testRes = camera.convert2WorldSpace(point);
+        expect(testRes.x).toBeCloseTo(30 - (800 / Math.sqrt(2)));
+        expect(testRes.y).toBeCloseTo(50);
+    });
+    
 });
