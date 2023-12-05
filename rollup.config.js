6a88b5ac2713aca664d859630f06d0015a0ad6da
niuee <vntchang@gmail.com>
2023-12-05 13:02:47 +0800

    Add commonjs build config in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -8,12 +8,12 @@ const packageJson = require("./package.json");
 
 export default [{
   input: 'src/index.ts',
-  output: [//{
-  //   file: packageJson.main,
-  //   format: 'cjs',
-  //   name: 'YourLibrary',
-  //   sourcemap: true,
-  // },  
+  output: [{
+    file: packageJson.main,
+    format: 'cjs',
+    name: 'YourLibrary',
+    sourcemap: true,
+  },  
   {
     file: packageJson.module,
     format: 'esm',
