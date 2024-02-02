7df5f9e5c0b9fa03e653b548d18ef90f893ebbe4
Vincent Chang <vntchang@gmail.com>
2024-02-02 14:45:52 +0800

    Add a copy readme file function to the build directory in build script

--- scripts/move-package.mjs
+++ scripts/move-package.mjs
@@ -4,5 +4,7 @@ data.module = "./index.js";
 data.types = "./index.d.ts";
 data.scripts = { test: "echo \"Error: no test specified\" && exit 1"};
 
-import { writeFileSync } from "fs";
+import { writeFileSync, copyFileSync } from "fs";
 writeFileSync("./build/package.json", JSON.stringify(data, null, 2));
+
+copyFileSync("./README.md", "./build/README.md");
