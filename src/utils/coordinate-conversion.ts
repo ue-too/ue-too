917f32fb08bda6b9b4316a5c807386e5a51a513b
Vincent Chang <vntchang@gmail.com>
2025-06-10 12:07:11 +0800

    feat: add z to the coordinate conversion

--- src/utils/coordinate-conversion.ts
+++ src/utils/coordinate-conversion.ts
@@ -7,6 +7,6 @@ export function pointConversion(point: Point) {
 
     return {
         x: point.x * cos30 - point.y * cos30,
-        y: point.x * cos60 + point.y * cos60
+        y: point.x * cos60 + point.y * cos60 + point.z
     }
 }
