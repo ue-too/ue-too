8a20d77d9eed6ab057f53b678208225f05c59b69
Vincent Chang <vincent.yy.chang@myviewboard.com>
2025-07-25 13:56:14 +0800

    wip

---
+++ apps/placeholder/src/index.ts
@@ -0,0 +1,8 @@
+import { libA } from "@ue-too/lib-a";
+
+libA();
+
+export function placeholderApp() {
+  console.log("placeholderApp");
+}
+
---
+++ packages/lib-a/src/index.ts
@@ -0,0 +1,3 @@
+export function libA(): void {
+  console.log("libA");
+}
