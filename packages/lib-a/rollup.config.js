502f357d19c2fad6811bf000172e30793eed189a
Vincent Chang <vincent.yy.chang@myviewboard.com>
2025-07-25 17:46:46 +0800

    wip

---
+++ packages/lib-a/rollup.config.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+	input: 'src/index.ts',
+	output: {
+		file: 'dist/index.js',
+		format: 'esm',
+        sourcemap: true
+	},
+    plugins: [
+        typescript({
+            tsconfig: 'tsconfig.json',
+            outputToFilesystem: true
+        })
+    ]
+};
