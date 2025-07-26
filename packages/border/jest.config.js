33c95fe1aa9ffd2f0a6f81ee3833b1a6ee651757
niuee <vntchang@gmail.com>
2025-07-26 09:35:15 +0800

    add border

---
+++ packages/border/jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: 'tsconfig.spec.json'
+    }]
+  },
+  moduleNameMapper: {
+    '^@ue-too/(.*)$': '<rootDir>/../$1/src'
+  }
+}; 
