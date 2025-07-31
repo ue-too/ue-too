9c366fb39504da5850abaa7c65b0865e848c2e39
niuee <vntchang@gmail.com>
2025-07-31 15:52:03 +0800

    feat: ecs coordinator

--- packages/ecs/test/index.test.ts
+++ packages/ecs/test/index.test.ts
@@ -1,4 +1,4 @@
-import { ComponentArray, EntityManager, ComponentManager } from "../src";
+import { ComponentArray, EntityManager, ComponentManager, System, SystemManager, Coordinator, Entity } from "../src";
 
 type MockComponent = {
     name: string;
@@ -64,6 +64,75 @@ describe('ComponentManager', () => {
         const componentManager = new ComponentManager();
         componentManager.registerComponent<MockComponent>('MockComponent');
         expect(componentManager.getComponentType('MockComponent')).toBe(0);
+    });
+
+});
+
+describe('Coordinator', () => {
+
+    it('should be able to create an entity', () => {
+        const coordinator = new Coordinator();
+        const entity = coordinator.createEntity();
+        expect(entity).toBeDefined();
+        // the first entity
+        expect(entity).toBe(0);
+    });
+
+    it('should be able to register a component', () => {
+        const coordinator = new Coordinator();
+        coordinator.registerComponent<MockComponent>('MockComponent');
+        expect(coordinator.getComponentType('MockComponent')).toBe(0);
+    });
+
+    it('should be able to add a component to an entity', () => {
+        const coordinator = new Coordinator();
+        coordinator.registerComponent<MockComponent>('MockComponent');
+        const entity = coordinator.createEntity();
+        const component = {name: 'John', age: 30};
+        coordinator.addComponentToEntity('MockComponent', entity, component);
+        expect(coordinator.getComponentFromEntity('MockComponent', entity)).toEqual(component);
+    });
+
+    it('should be able to remove a component from an entity', () => {
+        const coordinator = new Coordinator();
+        coordinator.registerComponent<MockComponent>('MockComponent');
+        const entity = coordinator.createEntity();
+        const component = {name: 'John', age: 30};
+        coordinator.addComponentToEntity('MockComponent', entity, component);
+        coordinator.removeComponentFromEntity('MockComponent', entity);
+        expect(coordinator.getComponentFromEntity('MockComponent', entity)).toBeNull();
+    });
+
+    it('should be able to set a system signature and update the system entities', () => {
+        const coordinator = new Coordinator();
+        coordinator.registerComponent<MockComponent>('MockComponent');
+        const mockSystem = {entities: new Set<Entity>()};
+        coordinator.registerSystem('MockSystem', mockSystem);
+        coordinator.setSystemSignature('MockSystem', 1);
+        const entity = coordinator.createEntity();
+        coordinator.addComponentToEntity('MockComponent', entity, {name: 'John', age: 30});
+        coordinator.setSystemSignature('MockSystem', 1);
+        expect(mockSystem.entities.size).toBe(1);
+        expect(mockSystem.entities.has(entity)).toBe(true);
+    });
+
+    it('should be able to let system iterate over its entities based on the system signature', () => {
+        const coordinator = new Coordinator();
+        coordinator.registerComponent<MockComponent>('MockComponent');
+        const mockSystem = {entities: new Set<Entity>()};
+        coordinator.registerSystem('MockSystem', mockSystem);
+        coordinator.setSystemSignature('MockSystem', 1);
+        const entity = coordinator.createEntity();
+        coordinator.createEntity();
+        const entity3 = coordinator.createEntity();
+
+        coordinator.addComponentToEntity('MockComponent', entity, {name: 'John', age: 30});
+        coordinator.addComponentToEntity('MockComponent', entity3, {name: 'Jim', age: 35});
+
+        const entities = Array.from(mockSystem.entities);
+        expect(entities.length).toBe(2);
+        expect(entities[0]).toBe(entity);
+        expect(entities[1]).toBe(entity3);
     });
 
 });
