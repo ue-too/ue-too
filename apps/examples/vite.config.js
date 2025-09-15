9d1ce18ecdf0f359e932fc55a4da33bd4de58d9c
niuee <vntchang@gmail.com>
2025-09-15 13:17:14 +0800

    chore: add build in ci pipeline

--- apps/examples/vite.config.js
+++ apps/examples/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
                 main: resolve(__dirname, "src/index.html"),
                 base: resolve(__dirname, "src/base/index.html"),
                 ruler: resolve(__dirname, "src/ruler/index.html"),
-                navigation: resolve(__dirname, "src/navigation/keyboard.html"),
+                navigation: resolve(__dirname, "src/navigation/index.html"),
                 "pixi-integration": resolve(__dirname, "src/pixi-integration/index.html"),
                 "konva-integration": resolve(__dirname, "src/konva-integration/index.html"),
                 "fabric-integration": resolve(__dirname, "src/fabric-integration/index.html"),
