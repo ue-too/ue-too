f76a2550c9bc2aaf1b701acee5121b08aea6d541
niuee <vntchang@gmail.com>
2025-09-19 15:24:35 +0800

    wip

--- apps/racetrack-maker/src/pages/RaceTrackBuilder.tsx
+++ apps/racetrack-maker/src/pages/RaceTrackBuilder.tsx
@@ -1,7 +1,19 @@
 import Board from "../components/Board";
+import { useCallback } from "react";
 
 export default function RaceTrackBuilder() {
+
+    const animationCallback = useCallback((timestamp: number, ctx: CanvasRenderingContext2D) => {
+        ctx.fillStyle = '#4CAF50';
+        ctx.fillRect(0, 0, 100, 100);
+        
+        ctx.fillStyle = '#2196F3';
+        ctx.beginPath();
+        ctx.arc(200, 200, 50, 0, 2 * Math.PI);
+        ctx.fill();
+    }, []);
+
     return (
-        <Board fullScreen />
+        <Board  fullScreen animationCallback={animationCallback}/>
     );
 }
