8ff7181541631785821e1c9f543a1d9de8dcde54
niuee <vntchang@gmail.com>
2025-10-21 12:57:52 +0800

    feat: initial setup of race track maker

---
+++ apps/racetrack-maker/vite.config.ts
@@ -0,0 +1,30 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/racetrack-maker',
+  server: {
+    port: 4201,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4301,
+    host: 'localhost',
+  },
+  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+}));
