88d3275ab5c991ca1af4d084d21900d95f029b2d
niuee <66865205+niuee@users.noreply.github.com>
2025-11-25 13:55:15 +0800

    fix: do not toggle on edge auto input for kmt disabled state (#279)

--- packages/board/src/input-interpretation/input-state-machine/kmt-input-state-machine.ts
+++ packages/board/src/input-interpretation/input-state-machine/kmt-input-state-machine.ts
@@ -250,11 +250,11 @@ export class DisabledState extends TemplateState<KmtInputEventMapping, KmtInputC
 
     uponEnter(context: KmtInputContext): void {
         context.canvas.setCursor(CursorStyle.DEFAULT);
-        context.toggleOnEdgeAutoCameraInput();
+        // context.toggleOnEdgeAutoCameraInput();
     }
 
     beforeExit(context: KmtInputContext): void {
-        context.toggleOffEdgeAutoCameraInput();
+        // context.toggleOffEdgeAutoCameraInput();
     }
 
     get eventReactions(): EventReactions<KmtInputEventMapping, KmtInputContext, KmtInputStates> {
