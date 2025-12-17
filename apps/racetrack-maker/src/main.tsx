319a3d21532b5fae6a360941665003ca7532c92a
niuee <vntchang@gmail.com>
2025-12-17 11:33:35 +0800

    wip

--- apps/racetrack-maker/src/main.tsx
+++ apps/racetrack-maker/src/main.tsx
@@ -1,19 +1,19 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {Home} from "./pages/Home";
-import RaceTrackBuilder from './pages/RaceTrackBuilder';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import RaceTrackBuilder from "./pages/RaceTrackBuilder";
 
 const Index = () => {
   return (
-      <Router>
-         <Routes>
-              <Route path="/racetrack-maker/" element={<Home/>}/>
-              <Route path="/racetrack-maker/builder" element={<RaceTrackBuilder/>}/>
-          </Routes> 
-      </Router>
-  ); 
+    <Router>
+      <Routes>
+        <Route path="/racetrack-maker/" element={<Home />} />
+        <Route path="/racetrack-maker/builder" element={<RaceTrackBuilder />} />
+      </Routes>
+    </Router>
+  );
 };
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
-root.render(<Index/>);
+root.render(<Index />);
