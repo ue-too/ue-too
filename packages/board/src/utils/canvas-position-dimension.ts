ef9af948829891703e57d2bb4bbb65cea6986f07
niuee <66865205+niuee@users.noreply.github.com>
2026-01-02 12:48:31 +0800

    fix: update when attribute changes (#304)

--- packages/board/src/utils/canvas-position-dimension.ts
+++ packages/board/src/utils/canvas-position-dimension.ts
@@ -421,25 +421,25 @@ export class CanvasPositionDimensionPublisher {
         for(let mutation of mutationsList){
             if(mutation.type === "attributes"){
                 if(mutation.attributeName === "width"){
-                    // const canvas = mutation.target as HTMLCanvasElement;
+                    const canvas = mutation.target as HTMLCanvasElement;
                     // canvas.style.width = canvas.width / window.devicePixelRatio + "px";
-                    // const newRect = canvas.getBoundingClientRect();
-                    // const trueRect = getTrueRect(newRect, window.getComputedStyle(canvas));
-                    // if (rectChanged(this.lastRect, trueRect)) {
-                    //     this.publishPositionUpdate(trueRect);
-                    //     this.lastRect = trueRect;
-                    // }
+                    const newRect = canvas.getBoundingClientRect();
+                    const trueRect = getTrueRect(newRect, window.getComputedStyle(canvas));
+                    if (rectChanged(this.lastRect, trueRect)) {
+                        this.publishPositionUpdate(trueRect);
+                        this.lastRect = trueRect;
+                    }
                 } else if(mutation.attributeName === "height"){
-                    // const canvas = mutation.target as HTMLCanvasElement;
+                    const canvas = mutation.target as HTMLCanvasElement;
                     // canvas.style.height = canvas.height / window.devicePixelRatio + "px";
-                    // const newRect = canvas.getBoundingClientRect();
-                    // const trueRect = getTrueRect(newRect, window.getComputedStyle(canvas));
-                    // if (rectChanged(this.lastRect, trueRect)) {
-                    //     this.publishPositionUpdate(trueRect);
-                    //     this.lastRect = trueRect;
-                    // }
+                    const newRect = canvas.getBoundingClientRect();
+                    const trueRect = getTrueRect(newRect, window.getComputedStyle(canvas));
+                    if (rectChanged(this.lastRect, trueRect)) {
+                        this.publishPositionUpdate(trueRect);
+                        this.lastRect = trueRect;
+                    }
                 } else if (mutation.attributeName === "style"){
-                    // const canvas = mutation.target as HTMLCanvasElement;
+                    const canvas = mutation.target as HTMLCanvasElement;
                     // const styleWidth = parseFloat(canvas.style.width);
                     // const styleHeight = parseFloat(canvas.style.height);
                     // const newWidth = styleWidth * window.devicePixelRatio;
@@ -450,12 +450,12 @@ export class CanvasPositionDimensionPublisher {
                     // if(newHeight != canvas.height){
                     //     canvas.height = newHeight;
                     // }
-                    // const newRect = canvas.getBoundingClientRect();
-                    // const trueRect = getTrueRect(newRect, window.getComputedStyle(canvas));
-                    // if (rectChanged(this.lastRect, trueRect)) {
-                    //     this.publishPositionUpdate(trueRect);
-                    //     this.lastRect = trueRect;
-                    // }
+                    const newRect = canvas.getBoundingClientRect();
+                    const trueRect = getTrueRect(newRect, window.getComputedStyle(canvas));
+                    if (rectChanged(this.lastRect, trueRect)) {
+                        this.publishPositionUpdate(trueRect);
+                        this.lastRect = trueRect;
+                    }
                 }
             }
         }
