3ed998af774cda88cfe104a3c6f1a9c76c1c26c2
niuee <vntchang@gmail.com>
2026-01-07 13:24:26 +0800

    fix: board-vue dependency update

--- apps/board-vue/src/app/BLayer.vue
+++ apps/board-vue/src/app/BLayer.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-    import { Test  } from "@ue-too/board-vue-adapter";
+    import { Test } from "@ue-too/board-vue-adapter";
 
 
 </script>
 
 <template>
-    <Test />
+    <Test test=""/>
 </template>
