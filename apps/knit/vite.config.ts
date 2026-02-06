22ac796d2da0e2f6398466d16ef503859ce6756d
niuee <66865205+niuee@users.noreply.github.com>
2026-02-06 10:32:21 +0800

    feat: add new project knit (#324)

---
+++ apps/knit/vite.config.ts
@@ -0,0 +1,37 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { join, resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [tailwindcss(), react()],
+    root: resolve(__dirname, './src'),
+    publicDir: resolve(__dirname, 'public'),
+    build: {
+        outDir: resolve(join(__dirname, '../../'), 'dist'),
+        emptyOutDir: true,
+        sourcemap: true,
+        assetsDir: 'assets',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
+    },
+    assetsInclude: [
+        '**/*.png',
+        '**/*.jpg',
+        '**/*.jpeg',
+        '**/*.gif',
+        '**/*.svg',
+        '**/*.webp',
+    ],
+    server: {
+        strictPort: false,
+    },
+});
