7340f7c8fa95b4247825d5032a958d6a759a7575
Vincent Chang <vntchang@gmail.com>
2026-02-13 01:06:29 +0800

    wip

--- apps/knit/routes/test.tsx
+++ apps/knit/routes/test.tsx
@@ -1,15 +1,36 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useState } from 'react';
 
 import { Scroller } from '@/components';
+import { ScrollerWithTranslate } from '@/components/scroller/Scroller';
+import { Button } from '@/components/ui/button';
 
 export const Route = createFileRoute('/test')({
     component: TestComponent,
 });
 
+type ScrollerType = 'translate' | 'scroll';
+
 function TestComponent() {
+    const [scrollerType, setScrollerType] = useState<ScrollerType>('scroll');
+
     return (
-        <div className="flex h-full w-full items-center justify-center">
-            <Scroller />
-        </div>
+        <>
+            <div className="flex w-full items-center justify-center gap-4">
+                {scrollerType === 'scroll' && <Scroller />}
+                {scrollerType === 'translate' && <ScrollerWithTranslate />}
+            </div>
+            <div className="flex justify-center">
+                <Button
+                    onClick={() =>
+                        setScrollerType(prev =>
+                            prev === 'scroll' ? 'translate' : 'scroll'
+                        )
+                    }
+                >
+                    {scrollerType === 'scroll' ? 'Translate' : 'Scroll'}
+                </Button>
+            </div>
+        </>
     );
 }
--- apps/knit/src/components/scroller/Scroller.tsx
+++ apps/knit/src/components/scroller/Scroller.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+
+import { cn } from '@/lib/utils';
 
 import { Button } from '../ui/button';
 
@@ -34,6 +36,26 @@ const createIndices = (
     });
 };
 
+const createLoopingIndices = (startIndex: number, length: number) => {
+    const indices: number[] = [];
+    for (let i = startIndex; i <= length - 1; i++) {
+        indices.push(i);
+    }
+
+    for (let i = 0; i < startIndex; i++) {
+        indices.push(i);
+    }
+
+    return indices;
+};
+
+const createLoopingIndicesWithEndIndex = (endIndex: number, length: number) => {
+    const indices: number[] = [];
+    const startIndex = normalizeIndex(endIndex + 1, length);
+
+    return createLoopingIndices(startIndex, length);
+};
+
 const normalizeIndex = (index: number, length: number) => {
     // reduce the angle
     index = index % length;
@@ -41,6 +63,151 @@ const normalizeIndex = (index: number, length: number) => {
     // force it to be the positive remainder, so that 0 <= angle < 2 * Math.PI
     index = (index + length) % length;
     return index;
+};
+
+const smallerDistBetweenIndices = (
+    index1: number,
+    index2: number,
+    length: number
+) => {
+    const dist1 = Math.abs(index1 - index2);
+    const dist2 = length - dist1;
+    return Math.min(dist1, dist2);
+};
+
+export const ScrollerWithTranslate = () => {
+    const half = Math.floor(VISIBLE_COUNT / 2);
+    const [centerIndex, setCenterIndex] = useState(0);
+    const [startIndex, setStartIndex] = useState(0);
+
+    const indices = useMemo(() => {
+        return createLoopingIndices(startIndex, options.length);
+    }, [centerIndex, options.length]);
+
+    const indicesWithEndIndex = useMemo(() => {
+        return createLoopingIndicesWithEndIndex(startIndex - 1, options.length);
+    }, [centerIndex, options.length]);
+
+    const smallerDist = smallerDistBetweenIndices(
+        normalizeIndex(centerIndex - half, options.length),
+        startIndex,
+        options.length
+    );
+
+    console.log('startIndex', startIndex);
+    console.log('centerIndex', centerIndex);
+    console.log(indices);
+
+    const translateAmount = -(centerIndex - half) * ITEM_HEIGHT;
+    const translate = `translateY(${translateAmount}px)`;
+    const translateAmountWithEndIndex =
+        -(centerIndex - half - options.length) * ITEM_HEIGHT;
+    const translateWithEndIndex = `translateY(${translateAmountWithEndIndex}px)`;
+
+    return (
+        <div className="flex h-full flex-col items-center justify-center">
+            <h1>Scroller with translate</h1>
+            <div
+                className={`flex flex-col overflow-hidden`}
+                style={{ height: VISIBLE_COUNT * ITEM_HEIGHT }}
+            >
+                <div
+                    style={{
+                        height: VISIBLE_COUNT * ITEM_HEIGHT,
+                        transform: translate,
+                        transition: 'transform 0.25s ease-out',
+                    }}
+                >
+                    {indicesWithEndIndex.map((index, i) => {
+                        const option = options[index];
+                        if (index === centerIndex) {
+                            return (
+                                <div
+                                    key={`${i}-${option}-${index}-with-end-index`}
+                                    className="flex shrink-0 items-center justify-center bg-amber-100 text-red-500 select-none"
+                                    style={{ height: ITEM_HEIGHT }}
+                                    onClick={() => setCenterIndex(index)}
+                                >
+                                    {option}
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div
+                                    key={`${i}-${option}-${index}-with-end-index`}
+                                    className="flex shrink-0 items-center justify-center select-none"
+                                    style={{ height: ITEM_HEIGHT }}
+                                    onClick={() => setCenterIndex(index)}
+                                >
+                                    {option}
+                                </div>
+                            );
+                        }
+                    })}
+                </div>
+                <div
+                    style={{
+                        height: VISIBLE_COUNT * ITEM_HEIGHT,
+                        transform: translate,
+                        transition: 'transform 0.25s ease-out',
+                    }}
+                >
+                    {indices.map((index, i) => {
+                        const option = options[index];
+                        if (index === centerIndex) {
+                            return (
+                                <div
+                                    key={`${i}-${option}-${index}`}
+                                    className="flex shrink-0 items-center justify-center bg-amber-100 text-red-500 select-none"
+                                    style={{ height: ITEM_HEIGHT }}
+                                    onClick={() => setCenterIndex(index)}
+                                >
+                                    {option}
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div
+                                    key={`${i}-${option}-${index}`}
+                                    className="flex shrink-0 items-center justify-center select-none"
+                                    style={{ height: ITEM_HEIGHT }}
+                                    onClick={() => setCenterIndex(index)}
+                                >
+                                    {option}
+                                </div>
+                            );
+                        }
+                    })}
+                </div>
+            </div>
+            <Button
+                onClick={() => {
+                    setCenterIndex(prevCenter => {
+                        const normalizedNewCenter = normalizeIndex(
+                            prevCenter + 1,
+                            options.length
+                        );
+                        return normalizedNewCenter;
+                    });
+                }}
+            >
+                Next
+            </Button>
+            <Button
+                onClick={() => {
+                    setCenterIndex(prevCenter => {
+                        const normalizedNewCenter = normalizeIndex(
+                            prevCenter - 1,
+                            options.length
+                        );
+                        return normalizedNewCenter;
+                    });
+                }}
+            >
+                Previous
+            </Button>
+        </div>
+    );
 };
 
 export const Scroller = () => {
@@ -92,7 +259,7 @@ export const Scroller = () => {
     }, []);
 
     return (
-        <div className="flex h-full w-full flex-col items-center justify-center">
+        <div className="flex h-full flex-col items-center justify-center">
             <h1>Scroller</h1>
             <div
                 className={`flex flex-col overflow-hidden`}
